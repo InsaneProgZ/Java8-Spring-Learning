9bdbb57971924ae831afaf98e370beb69fa3055b
Yan Zingra <yan.zingra12@gmail.com>
2021-01-31 03:09:02 -0300

    YAN-001 : Testando novo payload.

---
+++ Learning/src/main/java/YanZingra/Learning/Data.java
@@ -0,0 +1,37 @@
+package YanZingra.Learning;
+
+public class Data {
+    public String name;
+    String function;
+    Integer age;
+
+    public Data (String name, String function, Integer age){
+        this.name=name;
+        this.function=function;
+        this.age=age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFunction() {
+        return function;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setFunction(String function) {
+        this.function = function;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
--- Learning/src/main/java/YanZingra/Learning/LearningApplication.java
+++ Learning/src/main/java/YanZingra/Learning/LearningApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LearningApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(LearningApplication.class, args);
+		System.out.println("Test");
 	}
 
 }
